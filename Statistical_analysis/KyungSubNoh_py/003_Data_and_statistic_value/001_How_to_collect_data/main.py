8b1590b7aff6efa9687eb70c36379e6b6b5945dc
youngminpark2559 <youngminpark2559@gmail.com>
2019-07-16 06:46:31 +0900

    Update

---
+++ Statistical_analysis/KyungSubNoh_py/003_Data_and_statistic_value/001_How_to_collect_data/main.py
@@ -0,0 +1,6 @@
+# conda activate py36gputorch100 && \
+# cd /mnt/1T-5e7/mycodehtml/statistics_analysis/Statistical_analysis/KyungSubNoh_py/003_Data_and_statistic_value/001_How_to_collect_data && \
+# rm e.l && python main.py \
+# 2>&1 | tee -a e.l && code e.l
+
+# ================================================================================
