47553714480fcef17c1aa4f8cb1c9fefd2b8e6a6
youngminpark2559 <youngminpark2559@gmail.com>
2019-07-16 20:26:51 +0900

    Update

---
+++ Statistical_analysis/KyungSubNoh_py/007_Testing_hypothesis/001_Test_hypothesis_P-value/main.py
@@ -0,0 +1,27 @@
+# conda activate py36gputorch100 && \
+# cd /mnt/1T-5e7/mycodehtml/statistics_analysis/Statistical_analysis/KyungSubNoh_py/007_Testing_hypothesis/001_Test_hypothesis_P-value && \
+# rm e.l && python main.py \
+# 2>&1 | tee -a e.l && code e.l
+
+# ================================================================================
+import numpy as np
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_20:12:01.png
+
+# /home/young/Pictures/2019_07_16_20:14:34.png
+
+# /home/young/Pictures/2019_07_16_20:20:25.png
+
+# /home/young/Pictures/2019_07_16_20:21:20.png
+
+# /home/young/Pictures/2019_07_16_20:22:16.png
+
+# /home/young/Pictures/2019_07_16_20:23:24.png
+
+# /home/young/Pictures/2019_07_16_20:24:29.png
+
+# /home/young/Pictures/2019_07_16_20:25:50.png
+
+
+
