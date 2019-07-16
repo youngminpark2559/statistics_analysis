5b5877c8517e6188eed0adf7498ce13b1f6609ee
youngminpark2559 <youngminpark2559@gmail.com>
2019-07-16 10:48:59 +0900

    Update

---
+++ Statistical_analysis/KyungSubNoh_py/004_Probability_and_statistics/001_Understand_probability/main.py
@@ -0,0 +1,85 @@
+# conda activate py36gputorch100 && \
+# cd /mnt/1T-5e7/mycodehtml/statistics_analysis/Statistical_analysis/KyungSubNoh_py/004_Probability_and_statistics/001_Understand_probability && \
+# rm e.l && python main.py \
+# 2>&1 | tee -a e.l && code e.l
+
+import numpy as np
+
+# ================================================================================
+# Purpose of statistics
+# - Extract sample data from population
+# - Inference parameters of population via sample data
+
+# Why you inference parameters of population via sample data?
+# - It's generally impossible to directly calculate parameters of population 
+
+# ================================================================================
+# Why statistics uses probability?
+# - Even good statistics can't make 100% accurate result
+# So, crebility of statistics is represented by probability
+
+# - High credibility: high probability of true
+
+# ================================================================================
+# Characteristics of probability
+
+# 1. $$$\sum\limits_{i=1}^{n}P(E_i)=1$$$
+# E: event
+# i: number of trials
+# P: probability of event occuring
+
+probs_of_dice=[1/6,1/6,1/6,1/6,1/6,1/6]
+# print("probs_of_dice",probs_of_dice)
+# [0.16666666666666666, 0.16666666666666666, 0.16666666666666666, 0.16666666666666666, 0.16666666666666666, 0.16666666666666666]
+
+sum_of_all_probs=round(np.sum(probs_of_dice),1)
+# print("sum_of_all_probs",sum_of_all_probs)
+# 1.0
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_09:39:57.png
+
+# /home/young/Pictures/2019_07_16_09:39:45.png
+
+# event_A: multiples of 2
+# event_B: multiples of 3
+# P(A and B)
+
+U_set=[1,2,3,4,5,6,7,8,9,10]
+number_of_U_set_element=len(U_set)
+
+A_event_set=[2,4,6,8,10]
+A_len=len(A_event_set)
+
+B_event_set=[3,6,9]
+B_len=len(B_event_set)
+
+A_and_B_event_set=[6]
+A_and_B_len=len(A_and_B_event_set)
+
+prob_A_occuring=A_len/number_of_U_set_element
+prob_B_occuring=B_len/number_of_U_set_element
+prob_A_and_B_ocurring=A_and_B_len/number_of_U_set_element
+
+prob_A_or_B_occuring=round(prob_A_occuring+prob_B_occuring-prob_A_and_B_ocurring,1)
+# print("prob_A_or_B_occuring",prob_A_or_B_occuring)
+# 0.7
+
+# /home/young/Pictures/2019_07_16_09:44:48.png
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_09:45:05.png
+
+# A and B events are independent
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_09:45:25.png
+
+
+
+
+
+
+
+
+
---
+++ Statistical_analysis/KyungSubNoh_py/004_Probability_and_statistics/003_Random_variable_expectation_value_and_variance/main.py
@@ -0,0 +1,121 @@
+# conda activate py36gputorch100 && \
+# cd /mnt/1T-5e7/mycodehtml/statistics_analysis/Statistical_analysis/KyungSubNoh_py/004_Probability_and_statistics/003_Random_variable_expectation_value_and_variance && \
+# rm e.l && python main.py \
+# 2>&1 | tee -a e.l && code e.l
+
+# ================================================================================
+import numpy as np
+from itertools import product
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_10:23:54.png
+
+all_possible_events=[1,2,3,4,5,6]
+mean_value=np.mean(all_possible_events)
+probability_of_each_event=[1/6,1/6,1/6,1/6,1/6,1/6]
+
+# ================================================================================
+# @ Characteristics of probability
+
+summed_probs=np.sum(probability_of_each_event)
+summed_probs=round(summed_probs,2)
+# print("summed_probs",summed_probs)
+# 1.0
+
+# ================================================================================
+# @ Expectation value
+
+multiplied=np.array(all_possible_events)*np.array(probability_of_each_event)
+# print("multiplied",multiplied)
+# [0.16666667 0.33333333 0.5        0.66666667 0.83333333 1.        ]
+
+summed=np.sum(multiplied)
+summed=round(summed,2)
+# print("summed",summed)
+# 3.5
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_10:27:35.png
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_10:29:09.png
+
+subtracted=np.array(all_possible_events)-np.array(mean_value)
+# print("subtracted",subtracted)
+# [-2.5 -1.5 -0.5  0.5  1.5  2.5]
+
+powered_by_2=np.power(subtracted,2)
+# print("powered_by_2",powered_by_2)
+# [6.25 2.25 0.25 0.25 2.25 6.25]
+
+multipled=powered_by_2*np.array(probability_of_each_event)
+# print("multipled",multipled)
+# [1.04166667 0.375      0.04166667 0.04166667 0.375      1.04166667]
+
+summed=multipled.sum()
+summed=round(summed,2)
+# print("summed",summed)
+# 2.92
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_10:32:36.png
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_10:33:26.png
+
+# ================================================================================
+std_of_RV=np.sqrt(summed)
+# print("std_of_RV",std_of_RV)
+# 1.7088007490635062
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_10:34:34.png
+
+incentives=[300,150,0,-100]
+prob_of_each_incentive_A=[0.58,0.87,0.55,0.05]
+prob_of_each_incentive_B=[0.65,0.51,0.45,0.05]
+
+# ================================================================================
+multiplied=np.array(incentives)*np.array(prob_of_each_incentive_A)
+# print("multiplied",multiplied)
+# [174.  130.5   0.   -5. ]
+expectation_value_from_A=np.sum(multiplied)
+# print("expectation_value_from_A",expectation_value_from_A)
+# 299.5
+
+multiplied=np.array(incentives)*np.array(prob_of_each_incentive_B)
+# print("multiplied",multiplied)
+# [174.  130.5   0.   -5. ]
+expectation_value_from_B=np.sum(multiplied)
+# print("expectation_value_from_B",expectation_value_from_B)
+# 266.5
+
+# print("Expectation value from A is larger than B?")
+# print(expectation_value_from_A>expectation_value_from_B)
+# True
+
+# ================================================================================
+subtracted_A=incentives-expectation_value_from_A
+# print("subtracted_A",subtracted_A)
+# [   0.5 -149.5 -299.5 -399.5]
+
+variance_A=np.power(subtracted_A,2)*prob_of_each_incentive_A
+# print("variance_A",variance_A)
+# [1.45000000e-01 1.94447175e+04 4.93351375e+04 7.98001250e+03]
+variance_A=round(np.sum(variance_A),2)
+# print("variance_A",variance_A)
+# 76760.01
+
+# ================================================================================
+subtracted_B=incentives-expectation_value_from_B
+# print("subtracted_B",subtracted_B)
+# [   0.5 -149.5 -299.5 -399.5]
+
+variance_B=np.power(subtracted_B,2)*prob_of_each_incentive_B
+# print("variance_B",variance_B)
+# [1.45000000e-01 1.94447175e+04 4.93351375e+04 7.98001250e+03]
+variance_B=round(np.sum(variance_B),2)
+# print("variance_B",variance_B)
+# 46327.44
+
+# ================================================================================
