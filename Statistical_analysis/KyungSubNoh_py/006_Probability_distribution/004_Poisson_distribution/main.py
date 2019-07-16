93c900426f0395c98d6ed9501e2cbe6fcdf4b0ba
youngminpark2559 <youngminpark2559@gmail.com>
2019-07-16 14:47:21 +0900

    Update

--- Statistical_analysis/KyungSubNoh_py/006_Probability_distribution/004_Poisson_distribution/main.py
+++ Statistical_analysis/KyungSubNoh_py/006_Probability_distribution/004_Poisson_distribution/main.py
@@ -9,6 +9,8 @@ import scipy.stats as st
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import math
+from scipy.stats import poisson
+import matplotlib.pyplot as plt
 
 # ================================================================================
 # Poisson distribution
@@ -33,8 +35,10 @@ import math
 
 # ================================================================================
 def calculate_poisson_distribution_probability(x,lambda_value):
-  prob_value=(lambda_value**x)*(np.exp**(-lambda_value))/factorial(x)
-  return prob_value
+  
+  
+  prob_value=(lambda_value**x)*(np.e**(-lambda_value))/math.factorial(x)
+  return round(prob_value,2)
 
 # ================================================================================
 lambda_value=3
@@ -42,7 +46,65 @@ lambda_value=3
 # ================================================================================
 # P(X<=1)
 
-result_prob=calculate_poisson_distribution_probability(x=1,lambda_value=lambda_value)
-print("result_prob",result_prob)
-afaf
+result_prob_x_1=calculate_poisson_distribution_probability(x=1,lambda_value=lambda_value)
+# print("result_prob_x_1",result_prob_x_1)
+# 0.15
 
+# ================================================================================
+result_prob_x_0=calculate_poisson_distribution_probability(x=0,lambda_value=lambda_value)
+# print("result_prob_x_0",result_prob_x_0)
+# 0.05
+
+# ================================================================================
+result_prob_x_less_than_1=result_prob_x_1+result_prob_x_0
+# print("result_prob_x_less_than_1",result_prob_x_less_than_1)
+# 0.2
+
+print(str(result_prob_x_less_than_1*100)+"%")
+# 20.0%
+
+# ================================================================================
+# Calculate following probability 
+# P(4<=x<=5)
+
+result_prob_x_4=calculate_poisson_distribution_probability(x=4,lambda_value=lambda_value)
+# print("result_prob_x_4",result_prob_x_4)
+# 0.17
+
+result_prob_x_5=calculate_poisson_distribution_probability(x=5,lambda_value=lambda_value)
+# print("result_prob_x_5",result_prob_x_5)
+# 0.1
+
+result_prob_x_4_5=result_prob_x_4+result_prob_x_5
+# print("result_prob_x_4_5",result_prob_x_4_5)
+# 0.27
+
+# ================================================================================
+lambda_vals=list(range(1,8))
+# print("lambda_vals",lambda_vals)
+# [1, 2, 3, 4, 5, 6, 7]
+
+x_vals=list(range(1,11))
+# print("x_vals",x_vals)
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+dict_for_poisson_dist={}
+list_for_poisson_dist=[]
+for one_lambda in lambda_vals:
+  for one_x in x_vals:
+    y_proba_val=calculate_poisson_distribution_probability(one_x,one_lambda)
+    # print("y_proba_val",y_proba_val)
+    list_for_poisson_dist.append(y_proba_val)
+  dict_for_poisson_dist[one_lambda]=list_for_poisson_dist
+
+# print("dict_for_poisson_dist",dict_for_poisson_dist)
+
+# ================================================================================
+plt.plot([dict_for_poisson_dist[1],dict_for_poisson_dist[2]])
+# plt.plot()
+# plt.plot(dict_for_poisson_dist[3])
+# plt.plot(dict_for_poisson_dist[4])
+# plt.plot(dict_for_poisson_dist[5])
+# plt.plot(dict_for_poisson_dist[6])
+# plt.plot(dict_for_poisson_dist[7])
+plt.show()
