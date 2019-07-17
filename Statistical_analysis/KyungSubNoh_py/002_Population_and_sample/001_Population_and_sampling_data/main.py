a7390f9987afee994123394953ecee0910a9bdd9
youngminpark2559 <youngminpark2559@gmail.com>
2019-07-17 23:00:35 +0900

    Update

--- Statistical_analysis/KyungSubNoh_py/002_Population_and_sample/001_Population_and_sampling_data/main.py
+++ Statistical_analysis/KyungSubNoh_py/002_Population_and_sample/001_Population_and_sampling_data/main.py
@@ -1,5 +1,5 @@
 # conda activate py36gputorch100 && \
-# cd /mnt/1T-5e7/mycodehtml/math/KSNOH-Statistics_py/002_Population_and_sample/001_Population_and_sampling_data && \
+# cd /mnt/1T-5e7/mycodehtml/statistics_analysis/Statistical_analysis/KyungSubNoh_py/002_Population_and_sample/001_Population_and_sampling_data && \
 # rm e.l && python main.py \
 # 2>&1 | tee -a e.l && code e.l
 
@@ -12,11 +12,11 @@
 # ================================================================================
 # ./pics/2019_07_15_20:36:47.png
 
-# Probability sampling method:
-# each sample has same probability of being selected
+# Probabilistic sampling method:
+# each sample data point has same probability of being selected
 
-# Non-probability sampling method
-# each sample has different probability of being selected
+# Non-probabilistic sampling method
+# each sample data point has different probability of being selected
 
 # ================================================================================
 # ./pics/2019_07_15_20:38:45.png
--- Statistical_analysis/KyungSubNoh_py/002_Population_and_sample/002_Distribution_of_sample_data/main.py
+++ Statistical_analysis/KyungSubNoh_py/002_Population_and_sample/002_Distribution_of_sample_data/main.py
@@ -22,14 +22,42 @@
 # ================================================================================
 # ./pics/2019_07_15_21:04:26.png
 
+# Standard normal distribution
+# = z distribution
+
+# When there is enough many sample
+# perform standardization on normal distribution
+# and get z distribution (standard normal distribution)
+
+# Use std of population
+
 # ================================================================================
 # ./pics/2019_07_15_21:05:11.png
 
+# Use std (s) of sample
+
+# ================================================================================
 # ./pics/2019_07_15_21:06:02.png
 
+# ================================================================================
 # ./pics/2019_07_15_21:07:30.png
 
+# chi square distribution is induced from standard normal distribution
+
+# distribution of (z distribution)^2 
+
+# random variable X: $$$x_1, x_2, x_3, ..., x_n$$$
+# perform power of 2: $$$x_1^2, x_2^2, x_3^2, ..., x_n^2$$$
+# $$$x_1^2, x_2^2, x_3^2, ..., x_n^2$$$ consist of new random variable $$$\chi^2$$$
+
+# Distribution of $$$\chi^2$$$:
+# dof is n
+# $$$\chi^2$$$ distribution
+
+# More number of sample ---> normal distribution
+
+# ================================================================================
 # ./pics/2019_07_15_21:08:45.png
 
+# ================================================================================
 # ./pics/2019_07_15_21:09:19.png
-
