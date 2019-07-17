ffde6e837dee293ee3837c7ae86260f22edc450e
youngminpark2559 <youngminpark2559@gmail.com>
2019-07-17 19:10:10 +0900

    Update

---
+++ Statistical_analysis/KyungSubNoh_py/007_Testing_hypothesis/003_2_Test_hypothesis_on_variance_of_population/main.py
@@ -0,0 +1,16 @@
+# conda activate py36gputorch100 && \
+# cd /mnt/1T-5e7/mycodehtml/statistics_analysis/Statistical_analysis/KyungSubNoh_py/007_Testing_hypothesis/003_2_Test_hypothesis_on_variance_of_population && \
+# rm e.l && python main.py \
+# 2>&1 | tee -a e.l && code e.l
+
+# ================================================================================
+import numpy as np
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_22:34:32.png
+
+# /home/young/Pictures/2019_07_16_22:34:52.png
+
+# ================================================================================
+# /home/young/Pictures/2019_07_16_22:35:02.png
+
