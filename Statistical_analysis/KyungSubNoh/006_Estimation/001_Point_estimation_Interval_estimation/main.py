07fb0caf8a593a86ea9a737d400a716313a709e5
YoungMin Park <youngminpark2559@gmail.com>
2019-08-03 18:19:50 +0900

    Update main.py

--- Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/main.py
+++ Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/main.py
@@ -1,51 +1,108 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+   <HEAD>
+      <TITLE>My first HTML document</TITLE>
+      <style rel="stylesheet" type="text/css">
+body {
+ font-size: 20px;
+ 
+ margin-top: 50px;
+    margin-bottom: 50px;
+    margin-right: 80px;
+    margin-left: 100px;
+    
+    padding-top: 50px;
+    padding-bottom: 50px;
+    padding-right: 80px;
+    padding-left: 80px;
+    
+    line-height:35px;
+}
+/* img {
+ width:900px;
+} */
+</style>
+      <script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+    "HTML-CSS" : {
+        availableFonts : ["STIX"],
+        preferredFont : "STIX",
+        webFont : "STIX-Web",
+        imageFont : null
+    }
+});
+</script>
+     <script src="https://cdn.mathjax.org/mathjax/latest/MathJax.js" type="text/javascript">    
+    MathJax.Hub.Config({
+        HTML: ["input/TeX","output/HTML-CSS"],
+        TeX: { extensions: ["AMSmath.js","AMSsymbols.js"], 
+               equationNumbers: { autoNumber: "AMS" } },
+        extensions: ["tex2jax.js"],
+        jax: ["input/TeX","output/HTML-CSS"],
+        tex2jax: { inlineMath: [ ['$$$','$$$'] ],
+                   displayMath: [ ['$$$$','$$$$'] ],
+                   processEscapes: true },
+        "HTML-CSS": { availableFonts: ["TeX"],
+                      linebreaks: { automatic: true } }
+    });
+</script>
+   </HEAD>
+   <BODY>
+<xmp>
+This is personal study note
+Copyright and original reference:
+https://www.youtube.com/watch?v=sGQcDXzxsXo&list=PLsri7w6p16vulKaxOlNQFSWuyDuE9oEVP&index=2&t=0s
 
+================================================================================
+"Point estimation" and "interval estimation"
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_07:51:35.png' alt=''><xmp>
 
-
-# Point estimation and interval estimation
-# /home/young/Pictures/2019_07_22_07:51:35.png
-
-# ================================================================================
-estimate and estimator
+================================================================================
+"estimate" and "estimator"
 
 estimate: statistical value
 estimator: tool or function for statistical value
 
-# ================================================================================
+================================================================================
 Conditions for good point estimation
-/home/young/Pictures/2019_07_22_07:54:07.png
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_07:54:07.png' alt=''><xmp>
 
-# ================================================================================
-Why you use interval estimation, confidence interval
-/home/young/Pictures/2019_07_22_07:55:14.png
+================================================================================
+Why you use "interval estimation", "confidence interval"
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_07:55:14.png' alt=''><xmp>
 
-Interval of estimated parameters
-Confidence value: 95%
-Confidence intervale: area for 95%
+- min value < estimated parameter < max value
+- Above range is correct by 95% chance
 
-# ================================================================================
-Standard normal distribution
-/home/young/Pictures/2019_07_22_07:57:25.png
+================================================================================
+Standard normal distribution for confidence intervale
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_07:57:25.png' alt=''><xmp>
 
-# ================================================================================
+================================================================================
 Interval estimation example
-/home/young/Pictures/2019_07_22_07:59:42.png
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_07:59:42.png' alt=''><xmp>
 
-# ================================================================================
+================================================================================
 Estimate mean of population by using interval estimation
 condition: 90% of confidence value
-/home/young/Pictures/2019_07_22_08:00:56.png
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_08:00:56.png' alt=''><xmp>
 
-# ================================================================================
+================================================================================
 Estimate mean of population by using interval estimation
 condition: 95% of confidence value
-/home/young/Pictures/2019_07_22_08:02:28.png
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_08:02:28.png' alt=''><xmp>
 
-# ================================================================================
+================================================================================
 Estimate mean of population by using interval estimation
 condition: 99% of confidence value
-/home/young/Pictures/2019_07_22_08:03:10.png
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_08:03:10.png' alt=''><xmp>
 
-# ================================================================================
+================================================================================
 Confidence value 100%
-/home/young/Pictures/2019_07_22_08:03:31.png
 
+</xmp><img src='https://raw.githubusercontent.com/youngminpark2559/statistics_analysis/master/Statistical_analysis/KyungSubNoh/006_Estimation/001_Point_estimation_Interval_estimation/pics/2019_07_22_08:03:31.png' alt=''><xmp>
+      
+</xmp>
+   </BODY>
+</HTML>
